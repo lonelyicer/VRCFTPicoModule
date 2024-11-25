24f3d87e7d54fc4fc072fe9341531265e1f8c300
RingLo_ <lonelyicer@qq.com>
2024-11-25 23:35:18 +0800

    feat: rich module info

--- VRCFTPicoModule/Utils/DataPacketHelpers.cs
+++ VRCFTPicoModule/Utils/DataPacketHelpers.cs
@@ -6,8 +6,8 @@ namespace VRCFTPicoModule.Utils
     {
         public static T ByteArrayToStructure<T>(byte[] bytes, int offset = 0) where T : struct
         {
-            int size = Marshal.SizeOf(typeof(T));
-            IntPtr ptr = Marshal.AllocHGlobal(size);
+            var size = Marshal.SizeOf(typeof(T));
+            var ptr = Marshal.AllocHGlobal(size);
 
             try
             {
---
+++ VRCFTPicoModule/Utils/Localization.cs
@@ -0,0 +1,54 @@
+﻿using VRCFaceTracking.Core.Helpers;
+
+namespace VRCFTPicoModule.Utils;
+
+public class Localization
+{
+    private Dictionary<string, string>? _translations;
+
+    private Localization() { }
+
+    private static Localization LocInstance { get; } = new();
+
+    public static void Initialize(string languageCode)
+    {
+        LocInstance.LoadLanguageAsync(languageCode).GetAwaiter().GetResult();
+    }
+
+    private async Task LoadLanguageAsync(string languageCode)
+    {
+        var jsonContent = await LoadResourceAsync($"VRCFTPicoModule.Assets.Locales.{languageCode}.json")
+                          ?? await LoadResourceAsync("VRCFTPicoModule.Assets.Locales.en-US.json");
+
+        if (jsonContent != null)
+        {
+            _translations = await Json.ToObjectAsync<Dictionary<string, string>>(jsonContent);
+        }
+        else
+        {
+            _translations = new Dictionary<string, string>();
+        }
+    }
+    
+    private async Task<string?> LoadResourceAsync(string resourceName)
+    {
+        await using var stream = GetType().Assembly.GetManifestResourceStream(resourceName);
+        if (stream == null) return null;
+        using var reader = new StreamReader(stream);
+        return await reader.ReadToEndAsync();
+    }
+
+    private string GetTranslation(string key)
+    {
+        if (_translations != null && _translations.TryGetValue(key, out var translation))
+        {
+            return translation;
+        }
+        return key;
+    }
+    
+    public static string T(string key)
+    {
+        return LocInstance.GetTranslation(key);
+    }
+}
